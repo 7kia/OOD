1da77e3d9ba50f5a78cfc3e2d7869cc07cfa27ac
7kia <il7kia@yandex.ru>
2017-03-09 08:59:50 +0300

    Fix first task to second lab

--- Lab2/WeatherStation/WeatherData.h
+++ Lab2/WeatherStation/WeatherData.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <string>
 #include "Observer.h"
 
 using namespace std;
@@ -31,37 +32,76 @@ private:
 	}
 };
 
-class CStatsDisplay : public IObserver<SWeatherInfo>
-{
-private:
-	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
-	Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
-	остается публичным
-	*/
-	void Update(SWeatherInfo const& data) override
-	{
-		if (m_minTemperature > data.temperature)
-		{
-			m_minTemperature = data.temperature;
-		}
-		if (m_maxTemperature < data.temperature)
-		{
-			m_maxTemperature = data.temperature;
-		}
-		m_accTemperature += data.temperature;
-		++m_countAcc;
 
-		std::cout << "Max Temp " << m_maxTemperature << std::endl;
-		std::cout << "Min Temp " << m_minTemperature << std::endl;
-		std::cout << "Average Temp " << (m_accTemperature / m_countAcc) << std::endl;
+
+class CWeatherStatistic
+{
+
+public:
+	CWeatherStatistic(std::string const& statisticName)
+		: m_statisticName(statisticName)
+	{
+	}
+
+	void Update(double const& newValue)
+	{
+		if (m_minValue > newValue)
+		{
+			m_minValue = newValue;
+		}
+		if (m_maxValue < newValue)
+		{
+			m_maxValue = newValue;
+		}
+
+		m_accValue += newValue;
+		++m_countAcc;
+	}
+
+	void PrintStatistics() const
+	{
+		std::cout << "- " << m_statisticName << std::endl;
+		std::cout << "Max: " << m_maxValue << std::endl;
+		std::cout << "Min: " << m_minValue << std::endl;
+		std::cout << "Average: " << m_accValue / m_countAcc << std::endl;
 		std::cout << "----------------" << std::endl;
 	}
 
-	double m_minTemperature = std::numeric_limits<double>::infinity();
-	double m_maxTemperature = -std::numeric_limits<double>::infinity();
-	double m_accTemperature = 0;
-	unsigned m_countAcc = 0;
+private:
+	double m_minValue = std::numeric_limits<double>::infinity();
+	double m_maxValue = -std::numeric_limits<double>::infinity();
+	double m_accValue = 0.f;
+	size_t m_countAcc = 0;
+	std::string m_statisticName;
+};
 
+
+
+class CStatsDisplay : public IObserver<SWeatherInfo>
+{
+public:
+	CStatsDisplay()
+		: m_temperatureStat("Temperature")
+		, m_humidityStat("Humidity")
+		, m_pressureStat("Pressure")
+	{
+	}
+
+private:
+	void Update(SWeatherInfo const& data) override
+	{
+		m_temperatureStat.Update(data.temperature);
+		m_humidityStat.Update(data.humidity);
+		m_pressureStat.Update(data.pressure);
+
+		m_temperatureStat.PrintStatistics();
+		m_humidityStat.PrintStatistics();
+		m_pressureStat.PrintStatistics();
+	}
+
+	CWeatherStatistic m_temperatureStat;
+	CWeatherStatistic m_humidityStat;
+	CWeatherStatistic m_pressureStat;
 };
 
 class CWeatherData : public CObservable<SWeatherInfo>
--- Lab2/WeatherStation/main.cpp
+++ Lab2/WeatherStation/main.cpp
@@ -11,15 +11,15 @@ int main()
 
 	CStatsDisplay statsDisplay;
 	wd.RegisterObserver(statsDisplay, 1);
-	CStatsDisplay statsDisplay2;
-	wd.RegisterObserver(statsDisplay2, 1);
-	CStatsDisplay statsDisplay3;
-	wd.RegisterObserver(statsDisplay, 2);
+	//CStatsDisplay statsDisplay2;
+	//wd.RegisterObserver(statsDisplay2, 1);
+	//CStatsDisplay statsDisplay3;
+	//wd.RegisterObserver(statsDisplay, 2);
 
 	wd.SetMeasurements(3, 0.7, 760);
 
 	std::cout << std::endl;
-	wd.RemoveObserver(statsDisplay);
+	//wd.RemoveObserver(statsDisplay);
 
 	wd.SetMeasurements(10, 0.8, 761);
 
