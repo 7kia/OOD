9a49ca30c4bf977e07868b3632d39e9c4690ac50
7kia <il7kia@yandex.ru>
2017-06-25 21:41:32 +0300

    Add carcass for group tests

---
+++ Lab7/composite_lib/Tests/TestingFunction.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <math.h>
+#include <iomanip>
+
+namespace
+{
+	template <typename Ex, typename Fn>
+	void VerifyException(Fn && fn, const std::string & expectedDescription)
+	{
+		BOOST_CHECK_EXCEPTION(
+			fn(), 
+			Ex, 
+			[&](const Ex& e) {
+				BOOST_CHECK_EQUAL(e.what(), expectedDescription);
+				return e.what() == expectedDescription;
+			}
+		);
+	}
+
+}
---
+++ Lab7/composite_lib/Tests/composite_lib_tests.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <boost/mpl/list.hpp>
+#include <boost/mpl/vector.hpp>
+
+#include <memory>
+
+#include "Group.h"
+#include "LineStyle.h"
+#include "fillstyle.h"
+
+#include "TestingFunction.h"
